d37c862c099ef96b41c25b906826748839691760
Alex Radetsky <rad@rad.kiev.ua>
2010-09-10 18:53:54 +0300

    Already tested on 1000+1 sockets

--- smppsvrtst.pl
+++ smppsvrtst.pl
@@ -86,54 +86,63 @@ if ( $pdu->{status} == 0x00 ) {          ## STATUS
 	die "fail 4: single message was not acccepted. PDU->status must be 0x00. \n";
 }
 
-#my $t0 = [gettimeofday];
+my $t0 = [gettimeofday];
 
 # Test No. 5: Send 1000 SMS
-#for (my $cx = 0; $cx < 1000; $cx++ ) {
-#	$seq = $cli->submit_sm('destination_addr' => '380504139380', short_message => 'Test message from smppsvrtst.pl') or die;
-#	$pdu = $cli->read_pdu() or die;
-#	if ($pdu->{status} != 0x00 ) { ## STATUS
-#		die "fail 5: multiple message was not acccepted. PDU->status must be 0x00. \n";
-#	}
-#}
-#
-#my $elapsed = tv_interval ( $t0, [ gettimeofday ] );
-#my $smspersec = 1000/$elapsed;
-#print "ok 5: 1000 SMS per $elapsed seconds ~~ $smspersec \n";
-#
+
+my $cli3 = Net::SMPP->new_connect( 'localhost', port => 9900, smpp_version => 0x34, async => 1 ) or die;
+$seq = $cli3->bind_transceiver( system_id => 'test1000', password => 'secret1000' ) or die;
+
+for (my $cx = 0; $cx < 1000; $cx++ ) {
+	$seq = $cli3->submit_sm('destination_addr' => '380504139380', short_message => 'Test message from smppsvrtst.pl') or die;
+	$pdu = $cli3->read_pdu() or die;
+	if ($pdu->{status} != 0x00 ) { ## STATUS
+		die "fail 5: multiple message was not acccepted. PDU->status must be 0x00. \n";
+	}
+}
+
+
+# unbind and close 
+$cli3->unbind() or die; 
+
+
+my $elapsed = tv_interval ( $t0, [ gettimeofday ] );
+my $smspersec = 1000/$elapsed;
+print "ok 5: 1000 SMS per $elapsed seconds ~~ $smspersec \n";
+
+
 # Test No. 6: Receive SM
-#my $dbh = DBI->connect_cached( $dsn, $user, $password );
-#unless ( defined($dbh) ) {
-#	die "fail 6: can't connect to database. DSN: '$dsn'\n";
-#}
-#
-#my $sth = $dbh->prepare_cached("insert into messages ( msg_type, esme_id, src_addr, dst_addr, body, coding, udh, mwi, mclass, message_id, validity, deferred, registered_delivery, service_type, extra, received ) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? ) ");
-#$sth->execute( 'MO', 1, '0504139380', 'smppsvrtst.pl', conv_str_hex('bla-bla-bla'), 2, undef, undef, undef, '1010101010101010101', 1440, undef, undef, undef, undef, '2010-08-30 23:59:00' );
-#$pdu = $cli->read_pdu();
-#warn Dumper($pdu);
-#print "ok 6: receive SM\n";
-#
+my $dbh = DBI->connect_cached( $dsn, $user, $password );
+unless ( defined($dbh) ) {
+	die "fail 6: can't connect to database. DSN: '$dsn'\n";
+}
+
+my $sth = $dbh->prepare_cached("insert into messages ( msg_type, esme_id, src_addr, dst_addr, body, coding, udh, mwi, mclass, message_id, validity, deferred, registered_delivery, service_type, extra, received ) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? ) ");
+$sth->execute( 'MO', 1, '0504139380', 'smppsvrtst.pl', conv_str_hex('bla-bla-bla'), 2, undef, undef, undef, '1010101010101010101', 1440, undef, undef, undef, undef, '2010-08-30 23:59:00' );
+$pdu = $cli->read_pdu();
+warn Dumper($pdu);
+print "ok 6: receive SM\n";
+
 # Test No. 7. Insert 1000 SM to database
-#
-#$t0 = [ gettimeofday ];
-#for (my $cx = 0; $cx < 1000; $cx++ ) {
-#	$sth->execute( 'MO',1,'0504139380','smppsvrtst.pl',conv_str_hex('bla-bla-bla'),0,undef,undef,undef,'1010101010101010101',1440,undef,undef,undef,undef,'2010-08-30 23:59:00');
-#};
-#
-#$elapsed = tv_interval ( $t0, [ gettimeofday ] );
-#$smspersec = 1000/$elapsed;
-#print "ok 7: inserted 1000 SMS per $elapsed seconds ~~ $smspersec \n";
+
+$t0 = [ gettimeofday ];
+for (my $cx = 0; $cx < 1000; $cx++ ) {
+	$sth->execute( 'MO',1,'0504139380','smppsvrtst.pl',conv_str_hex('bla-bla-bla'),0,undef,undef,undef,'1010101010101010101',1440,undef,undef,undef,undef,'2010-08-30 23:59:00');
+};
+
+$elapsed = tv_interval ( $t0, [ gettimeofday ] );
+$smspersec = 1000/$elapsed;
+print "ok 7: inserted 1000 SMS per $elapsed seconds ~~ $smspersec \n";
 
 # Test No 8. Receive 1000 SM from SMPPD
 
-#$t0 = [ gettimeofday ];
-#for (my $cx = 0; $cx < 1000; $cx++) {
-#	$pdu = $cli->read_pdu();
-#}
-#$elapsed = tv_interval ( $t0, [ gettimeofday ] );
-#$smspersec = 1000/$elapsed;
-#print "ok 8: received 1000 SMS per $elapsed seconds ~~ $smspersec \n";
-
+$t0 = [ gettimeofday ];
+for (my $cx = 0; $cx < 1000; $cx++) {
+	$pdu = $cli->read_pdu();
+}
+$elapsed = tv_interval ( $t0, [ gettimeofday ] );
+$smspersec = 1000/$elapsed;
+print "ok 8: received 1000 SMS per $elapsed seconds ~~ $smspersec \n";
 
 # Test No 9. Sending enquire-lik 
 
@@ -174,6 +183,43 @@ if ( $pdu->{status} == 0x00 ) {          ## STATUS
 	warn Dumper ($pdu); 
     print "ok 12: SMS failed from INVALID.\n"; 
 }
+
+#Test No. 13 : Throttling 
+$seq = $cli->submit_sm( 'data_coding' => '2', 'source_addr' => 'smppsvrtst.pl', 'destination_addr' => '380504139380', short_message => 'Test message from smppsvrtst.pl' ) or die;
+$pdu = $cli->read_pdu() or die;
+$seq = $cli->submit_sm( 'data_coding' => '2', 'source_addr' => 'smppsvrtst.pl', 'destination_addr' => '380504139380', short_message => 'Test message from smppsvrtst.pl' ) or die;
+$pdu = $cli->read_pdu() or die;
+if ( $pdu->{status} == 0x58 ) {          ## STATUS
+    print "ok 13: SM failed for THROTTLING reason.\n";
+} else {
+    die "fail 13: TWO messages was acccepted, but bandwidth value is 1. \n";
+}
+
+# Test No. 14 : 1000 connects 
+
+print "Test No. 14. Creating 1000 connects and bind_transciever.\n"; 
+my @many_connects = (); 
+
+for (my $x = 0; $x < 1000; $x++) { 
+	my $smpp =  Net::SMPP->new_connect( 'localhost', port => 9900, smpp_version => 0x34, async => 1 ) or die; 
+	unless ( defined ($smpp) ) { 
+	    die "fail 14: failed connect to 127.0.0.1:9900 in $x connect : $!\n";
+	} 
+	my $smpp_seq = $smpp->bind_transceiver( system_id => 'test1000', password => 'secret1000' ) or die;
+    unless ( defined ($smpp_seq) ) { 
+		die "fail 14: failed bind_transciever() in $x connect : $!\n"; 
+	}
+	my $smpp_pdu = $smpp->read_pdu() or die;  
+	if ($smpp_pdu->{status} != 0x00) { 
+		die "fail 14: pdu->status != 0x00 in $x \n"; 
+	} 
+	push @many_connects,$smpp; 
+	print "already connected: ".($x+1)."\n"; 
+
+}
+
+print "Test No. 14. Complete. 1000 sockets created. \n"; 
+
 
 
 #############################################################################
